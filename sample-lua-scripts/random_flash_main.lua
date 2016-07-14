a42367b19746142a59b4da29233c2157070a5034
Tomarty <thomartell@gmail.com>
2016-07-13 18:15:17 -0700

    Removed invalid Lua
    Lua does not support tuple keys. I've never used this API so I'm not sure if it works, but the preexisting Lua code contained invalid syntax and would fail to compile. I also localized some global variables and replaced redundant statements with for loops.

--- sample-lua-scripts/random_flash_main.lua
+++ sample-lua-scripts/random_flash_main.lua
@@ -4,27 +4,29 @@ local Thread = clr.System.Threading.Thread
 function play_anim()
 	while true do
 		-- Everthing should be white (strobe effect!!!!)
-		c = Colore.Color.White
-		background = Colore.Color.Black
+		local c = Colore.Color.White
+		local background = Colore.Color.Black
 		-- set keyboard colour
 		Keyboard.SetPosition(math.random(0,5), math.random(1,18), c, true)
 		-- set mousepad colour
 		Mousepad.SetAll(background)
-		Mousepad[math.random(0,15)] = c
-		Mousepad[math.random(0,15)] = c
-		Mousepad[math.random(0,15)] = c
+		for i = 1, 3 do
+			Mousepad[math.random(0,15)] = c
+		end
 		-- set mouse colour
 		Mouse.SetAll(background)
-		Mouse[math.random(0,9), math.random(0,7)] = c
-		Mouse[math.random(0,9), math.random(0,7)] = c
-		Mouse[math.random(0,9), math.random(0,7)] = c
+		for i = 1, 3 do
+			--Mouse[math.random(0,9), math.random(0,7)] = c -- this line isn't valid lua
+			Mouse[math.random(0,9)]] = c
+		end
 	
 		-- set keypad colour
 		Keypad.SetAll(background)
-		Keypad[math.random(0,4),math.random(0,5)] = c
-		Keypad[math.random(0,4),math.random(0,5)] = c
-		Keypad[math.random(0,4),math.random(0,5)] = c
-
+		for i = 1, 3 do
+			--Keypad[math.random(0,4), math.random(0,5)] = c -- this line isn't valid lua
+			Keypad[math.random(0,4)] = c
+		end
+		
 		-- We don't want to spam the SDK, so throttle to 50ms
 		Thread.Sleep(50)
 	end
